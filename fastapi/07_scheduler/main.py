331e52f69cebbbc8028bf6340975d8724a5610e4
gnl2109 <gnl2109@gmail.com>
2026-09-09 16:17:39 +0900

    스케쥴러

---
+++ fastapi/07_scheduler/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+from scheduler import sch_start
+
+app = FastAPI()
+sch = sch_start()
+
+@app.get("/")
+@app.get("/start")
+async def start():
+    if not sch.running:
+        sch.start()
+    else:
+        sch.resume()
+    return {'msg':'scheduler 실행'}
+
+@app.get("/stop")
+async def stop():
+    sch.pause() # 일시 정지. 재실행 가능.
+    # sch.shutdown() # 완전히 닫는다. 재실행 불가.
+    return {'msg':'scheduler 정지'}
+
+@app.get("/pause/{task_id}")
+def pause_job(task_id:str):
+    sch.pause_job(task_id)
+    return {'msg':f'{task_id} 일시정지'}
+
+@app.get("/resume/{task_id}")
+def resume(task_id:str):
+    sch.resume_job(task_id)
+    return {'msg':f'{task_id} 다시실행'}
