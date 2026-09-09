953cde8875d9460133f076185e882974c2d314a9
gnl2109 <gnl2109@gmail.com>
2026-09-09 12:13:36 +0900

    전송준비, 파일업로드->이름변경

---
+++ fastapi/06_file_upload/main.py
@@ -0,0 +1,54 @@
+# uv pip install -r requirements.txt
+import logging
+import os
+import uuid
+from typing import List
+
+from fastapi import FastAPI, UploadFile
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
+from starlette.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# 일반 print 로그의 단점
+# 로그가 찍힌 시간, 위치 등을 알 수 없다.
+# DEBUG > INFO > WARNING > ERROR > CRITICAL
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(levelname)s:     [%(name)s] %(message)s - %(asctime)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+logger = logging.getLogger(__name__)
+
+logger.info("logger test!!")
+
+FILE_PATH = './upload'
+
+# 특정 경로에 폴더 생성
+if not os.path.exists(FILE_PATH):
+    os.makedirs(FILE_PATH)
+    logger.info(f"{FILE_PATH} 생성!")
+
+app.mount("/view", StaticFiles(directory="view"))
+app.mount("/images",StaticFiles(directory=FILE_PATH))
+app.add_middleware(CORSMiddleware,allow_origins=["*"],allow_methods=["*"])
+
+@app.get("/")
+def main():
+    return RedirectResponse("/view/upload.html")
+
+@app.post("/upload")
+def upload(files: List[UploadFile]):
+
+    for file in files:
+        logger.info(f'file name : {file.filename}') # img.png -> 123456788.png
+        ori_filename = file.filename
+        # 1. 파일명과 확장자 분리
+        name,ext = ori_filename.split('.')
+        logger.info(f'{name} / {ext}')
+        # 2. 파일명 변경
+        new_filename = f'{uuid.uuid4()}.{ext}'
+        logger.info(f'new file name = {new_filename}')
+        # 3. 새로운파일명 + 확장자
