ae6d453e17f1964179803575548c046a57950b76
gnl2109 <gnl2109@gmail.com>
2026-09-10 15:54:03 +0900

    대화 서비스 완성

---
+++ lang_chain/03_chat/ollama_service.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_ollama import ChatOllama
+
+router = APIRouter(prefix="/ask", tags=["ask"])
+
+# 모델 호출
+model = ChatOllama(model="exaone3.5:2.4b")
+
+conversation_history = [] # 대화저장 리스트
+
+# 프롬프트 작성
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "당신은 답변 전문 AI 모델 입니다. 주뭐진 질문에 대해서 핵심만 간단히 대답하세요"),
+    MessagesPlaceholder(variable_name="history"), # 대화내용을 history 라는 이름으로 줄게
+    ("user", "{query}")
+])
+
+# 파이프라인 조립
+chain = prompt | model
+
+# 실행 및 출력
+def chat_answer(query:str):
+    answer = ''
+    for chunk in chain.stream({'query':query,'history':conversation_history}):
+        #print(chunk.content,end='',flush=True) # StrOutputParser() 를 안써서 .content 붙이는 것
+        answer = chunk.content
+        yield chunk.content
+
+    conversation_history.append(HumanMessage(content=query))
+    conversation_history.append(AIMessage(content=answer))
+    print(f'history length : {len(conversation_history)}')
