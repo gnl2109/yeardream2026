17bbaa321bf77126d8719aebaa0c6c14641c377f
gnl2109 <gnl2109@gmail.com>
2026-09-16 10:51:05 +0900

    최종 완성

--- lang_graph/01_start/04_loop.py
+++ lang_graph/01_start/04_loop.py
@@ -74,7 +74,16 @@ def critic_node(state:WriteState) -> WriteState:
 
 def route_by_review(state:WriteState) -> str:
     """PASS / RETRY 에 따라서 다른 노드로 갈 수 있는 문자열을 반환"""
-    return "go_retry"
+
+    if state.state == 'PASS':
+        print('검증 통과')
+        return "go_end"
+    elif state.count >= 3:
+        print("3회 초과로 재시도 중지")
+        return "go_end"
+    else:
+        print(f"{state.count}회 시도 \n 피드백 : {state.feedback}")
+        return "go_retry"
 
 # 4. 저장소 등록
 wf = StateGraph(WriteState)
@@ -86,11 +95,18 @@ wf.add_node('critic',critic_node)
 # 6. 엣지 등록(조립)
 wf.set_entry_point('writer')
 wf.add_edge('writer','critic')
-wf.add_edge('critic',END) # END는 ctrl+space 하여 자동완성 선택(langgraph)
+wf.add_conditional_edges(
+    'critic',
+    route_by_review,
+    {
+        'go_end':END, # END는 ctrl+space 하여 자동완성 선택(langgraph)
+        'go_retry':'writer'
+    })
 
 # 7. 컴파일
 app = wf.compile()
 
 # 8. 실행
-result = app.invoke({'topic':'전기 자동차'})
-print(result)
+for node in app.stream({'topic':'전기 자동차'},stream_mode="updates"):
+    for key,val in node.items():
+        print(f"{key}:{val}")
