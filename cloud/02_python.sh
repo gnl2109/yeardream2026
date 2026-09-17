1206057bb79c02376f0d6fc6a4f47f84211d25ff
gnl2109 <gnl2109@gmail.com>
2026-09-17 13:58:12 +0900

    서버 백그라운드 실행

---
+++ cloud/02_python.sh
@@ -0,0 +1,52 @@
+# 1. python 설치
+sudo yum install -y python3.14 python3.14-pip
+python3.14 --version # 설치 확인
+
+# 현재 경로
+pwd
+# 현재 디렉토리의 리스트
+ls -al
+# 2. 디렉토리 생성
+mkdir app
+# 생성된 디렉토리로 들어가기
+cd app
+
+# 3. 가상환경 생성 및 실행
+python3.14 -m venv venv
+source venv/bin/activate
+
+# pip 업그레이드 / uv 설치
+pip install --upgrade pip
+pip install uv
+
+# requirements.txt 생성 및 수정 (vim편집기 사용)
+vim requirements.txt
+# i (텍스트편집) , o (한줄 띄우고 편집?)
+fastapi
+uvicorn
+# ESC -> :wq (저장하고 종료) , :q! (저장안하고 종료)
+cat requirements.txt
+
+# 라이브러리 설치
+uv pip install -r requirements.txt
+
+# 4. 실행
+uvicorn main:app --host=0.0.0.0 --port=8000 --workers 2
+
+# 멈추지 않고 실행하는 방법
+# nohup : 종료되지 않고 계속 실행할 수 있게 해준다.
+# > uvicorn.log 실행 내용을 uvicorn.log 로 남기겠다.
+# 2>&1 : 2는 에러로그 1은 표준출력로그 -> 에러로그도 표준출력로그처럼 출력해라
+# 2>1 로 하면 에러로그를 파일명 1에 저장하라고 오해할 수 있어 특수문자 &를 붙임
+# & : 백그라운드로 실행해라 -> 내 UI 가리지 마
+nohup uvicorn main:app --host=0.0.0.0 --port=8000 --workers 2 > uvicorn.log 2>&1 &
+
+# 로그확인 방법
+# 실시간
+tail -f uvicorn.log
+# 읽기
+cat uvicorn.log
+vim uvicorn.log
+
+# 가상환경 종료
+deactivate
