ecad18f3c9ad66ad3acd8e552016debbf7c9737e
gnl2109 <gnl2109@gmail.com>
2026-09-18 16:11:25 +0900

    데이터올리기+SELECT+트랜잭션 완료

--- sql/02_DML.sql
+++ sql/02_DML.sql
@@ -1,5 +1,5 @@
 /* 문자열 캐릭터셋 변경 (latin1 -> utf8mb4)
-sudo
+sudo vim /etc/my.cnf
 
 [mysqld]
 character-set-server = utf8mb4
@@ -9,6 +9,12 @@ collation-server = utf8mb4_unicode_ci
 sudo systemctl restart mariadb
 systemctl status mariadb
 */
+
+SHOW VARIABLES LIKE 'character_set%'
+
+-- 캐릭터셋이 바뀌기 전에 만들어져버린 database 와 table 에 대해서 변경
+ALTER DATABASE mydb CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+ALTER TABLE employees CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 USE mydb;
 DESC employees;
@@ -20,8 +26,36 @@ INSERT INTO employees(
 	111,'지훈','김','zerObox@naver.com','01020343054','90000000','dev001',90
 );
 
+INSERT INTO employees(
+	emp_no, first_name, family_name, email, mobile, salary
+)VALUES(
+	112,'태근','김','email@naver.com','01020343054','90000000'
+);
+
+INSERT INTO employees(
+	emp_no, family_name, email, mobile, salary
+)VALUES(
+	113,'김','zerObox@naver.com','01020343054','90000000'
+);
+
+SELECT * FROM employees e;
+
 -- UPDATE
+-- UPDATE [테이블] SET [컬럼]=[값] WHERE [조건]
+UPDATE employees SET depart_no = 'dev002' WHERE depart_no IS NULL;
+UPDATE employees SET commission = 10 WHERE commission IS NULL;
 
 -- DELETE
+-- DELETE FROM [테이블명] WHERE [조건]
+DELETE FROM employees WHERE first_name is null;
 
 -- UPSERT(나중에 자세하게...)
+-- 키가 중복되면 UPDATE, 중복되지 않으면 INSERT
+-- 키가 없으면 실행할 수 없다.
+INSERT INTO employees(emp_no, first_name, family_name, email, mobile, salary)
+	VALUES(112,'태근','김','email@naver.com','01012341234','50000000')
+		ON DUPLICATE KEY UPDATE first_name='태곤', falmily_name='박';
+
+
+
+
